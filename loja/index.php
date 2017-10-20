eb8f5a80062ded85a72917b657ed7751a645d70c
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-10-19 22:07:00 -0300

    foi modificado o local do form de login quando digita-se um login errado

--- loja/index.php
+++ loja/index.php
@@ -8,20 +8,21 @@
 <?php } ?>
 <?php if(isset($_GET['login']) && $_GET['login'] == false) { ?>
     <p class="alert-danger">Usuário ou senha inválida!</p>
+
+    <form action="login.php" method="post">
+        <table class="table">
+            <tr>
+                <td>Email</td>
+                <td><input class="form-control" type="email" name="email"></td>
+            </tr>
+            <tr>
+                <td>Senha</td>
+                <td><input class="form-control" type="password" name="senha"></td>
+            </tr>
+            <tr>
+                <td><button type="submit" class="btn btn-primary">Login</button></td>
+            </tr>
+        </table>
+    </form>
 <?php }?>
-<form action="login.php" method="post">
-    <table class="table">
-        <tr>
-            <td>Email</td>
-            <td><input class="form-control" type="email" name="email"></td>
-        </tr>
-        <tr>
-            <td>Senha</td>
-            <td><input class="form-control" type="password" name="senha"></td>
-        </tr>
-        <tr>
-            <td><button type="submit" class="btn btn-primary">Login</button></td>
-        </tr>
-    </table>
-</form>
 <?php include("rodape.php"); ?>
