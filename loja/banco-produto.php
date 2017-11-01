9f3694098f6fe1a604e5e8c71d0bccbac9530c21
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-10-31 21:43:45 -0300

    refatorando a listagem de produtos

--- loja/banco-produto.php
+++ loja/banco-produto.php
@@ -1,11 +1,20 @@
 <?php
 require_once('conecta.php');
+require_once ("class/Produto.php");
 
 function listaProdutos($conexao) {
     $produtos = array();
     $resultado = mysqli_query($conexao, "select p.*, c.nome as categoria_nome from produtos as p join categorias as c on p.categoria_id = c.id");
 
-    while($produto = mysqli_fetch_assoc($resultado)) {
+    while($produto_array = mysqli_fetch_assoc($resultado)) {
+        $produto = new Produto();
+
+        $produto->nome = $produto_array['nome'];
+        $produto->categoria_id = $produto_array['categoria_nome'];
+        $produto->descricao = $produto_array['descricao'];
+        $produto->preco = $produto_array['preco'];
+        $produto->usado = $produto_array['usado'];
+
         array_push($produtos, $produto);
     }
 
