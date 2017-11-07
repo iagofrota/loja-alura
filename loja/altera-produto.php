ff238a3b50b01aba4f7262fa1db9d19084a7ad40
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-07 20:50:23 -0300

    apaguei os require_once desnecessários

--- loja/altera-produto.php
+++ loja/altera-produto.php
@@ -1,8 +1,6 @@
 <?php
 require_once("cabecalho.php");
 require_once("banco-produto.php");
-require_once ("class/Produto.php");
-require_once ("class/Categoria.php");
 ?>
 
 <?php
--- loja/produto-altera-formulario.php
+++ loja/produto-altera-formulario.php
@@ -1,8 +1,6 @@
 <?php require_once("cabecalho.php");
 require_once("banco-categoria.php");
 require_once("banco-produto.php");
-require_once ("class/Produto.php");
-require_once ("class/Categoria.php");
 
 $id = $_GET['id'];
 $produto = buscaProduto($conexao, $id);
