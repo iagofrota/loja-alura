2aeef37408b182c140f7c045503ac74c27022cda
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-07 21:06:54 -0300

    cabeçalho atualizado

--- loja/cabecalho.php
+++ loja/cabecalho.php
@@ -1,11 +1,12 @@
 <?php
-error_reporting(E_ALL ^ E_NOTICE);
-require_once 'mostra-alerta.php';
-
 spl_autoload_register(function ($nomeDaClasse)
 {
     require_once ("class/" . $nomeDaClasse . '.php');
 });
+
+error_reporting(E_ALL ^ E_NOTICE);
+require_once 'mostra-alerta.php';
+require_once ("conecta.php");
 
 ?>
 <html>
