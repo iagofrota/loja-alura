3624aea9ce72546fab3b7e26d5623584d57e8449
Iago Olímpio Frota <iagofrota10@hotmail.com>
2017-11-07 21:21:05 -0300

    criação do CategoriaDao

---
+++ loja/class/CategoriaDao.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: iagof
+ * Date: 07/11/2017
+ * Time: 21:18
+ */
+require_once("conecta.php");
+
+class CategoriaDao
+{
+    private $conexao;
+
+    public function __construct($conexao)
+    {
+        $this->conexao = $conexao;
+    }
+
+    function listaCategorias()
+    {
+        $categorias = array();
+        $query = "SELECT * FROM categorias";
+        $resultado = mysqli_query($this->conexao, $query);
+
+        while ($categoria_array = mysqli_fetch_assoc($resultado)) {
+
+            $categoria = new Categoria();
+            $categoria->setId($categoria_array['id']);
+            $categoria->setNome($categoria_array['nome']);
+
+            array_push($categorias, $categoria);
+        }
+
+        return $categorias;
+    }
+}
